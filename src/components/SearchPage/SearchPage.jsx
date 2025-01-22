c230e010e3530a08e000efcdb29a9191ae0a243f
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-01-22 18:49:04 +0500

    SearchPage improvements; handleSubmit on searchQuery

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,10 +1,27 @@
 import styles from "./searchpage.module.css";
 
+import { useState } from "react";
+
 export default function SearchPage() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleSubmit = () => {
+    console.log(searchQuery);
+  };
   return (
     <div className={styles["search-page"]}>
       <div className={styles.headling}>Find Your Movie</div>
-      <input type="text" className={styles["search-bar"]} placeholder="Lord of the rings"></input>
+      <div className={styles["search-wrapper"]}>
+        <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          type="text"
+          className={styles["search-bar"]}
+          placeholder="Lord of the rings"
+        ></input>
+        <button onClick={handleSubmit} className={styles["submit-btn"]}>
+          Enter
+        </button>
+      </div>
     </div>
   );
 }
