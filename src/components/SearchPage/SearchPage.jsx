0187dc29df1ce90cf8c389888d971e88da766fc2
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-01-23 03:18:32 +0500

    Improvements on API call function for movies into the SearchPage element v2

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -6,15 +6,13 @@ import requestAPI from "../../common/services/movies";
 
 export default function SearchPage() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [queryResult, setQueryResult] = useState("");
-  const handleSubmit = () => {
-    requestAPI(searchQuery).then((data) => {
-      setQueryResult(data);
-    });
-    const newItem = document.createElement("div");
-    newItem.textContent = queryResult.data.Title;
-    document.querySelector("#search-result-id").append(newItem);
+  const [queryResult, setQueryResult] = useState(null);
+
+  const handleSubmit = async () => {
+    const data = await requestAPI(searchQuery);
+    setQueryResult(data);
   };
+
   return (
     <div className={styles["search-page"]}>
       <div className={styles.headling}>Find Your Movie</div>
@@ -29,7 +27,9 @@ export default function SearchPage() {
         <button onClick={handleSubmit} className={styles["submit-btn"]}>
           Enter
         </button>
-        <div id="search-result-id"></div>
+        <div id="search-result-id">
+          {queryResult ? queryResult.data.Title : ""}
+        </div>
       </div>
     </div>
   );
