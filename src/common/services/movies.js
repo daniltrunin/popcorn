5145a937f0464d834b1f5e7b54b092a616b31bc8
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-01-24 23:22:25 +0500

    Posters render on SearchPage

--- src/common/services/movies.js
+++ src/common/services/movies.js
@@ -3,17 +3,22 @@ import axios from "axios";
 const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDVkN2E5NWEwMDQ4NTE3NTc3MTUwZmY2NTZiODcwYSIsIm5iZiI6MTczNzYzNTQ5OS4yMTcsInN1YiI6IjY3OTIzNmFiN2FmMjM1MjZkNzQ3MDJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ruyQsJaYSdT3yTskcbhS_Q861fdQSXiMdDl5Fxncjk4";
 
 const requestAPI = function (title) {
-  return axios.get(
-    "https://api.themoviedb.org/3/search/movie",
-    {
-      params: {
-        query: `${title}`
-      },
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`
+  try {
+    return axios.get(
+      "https://api.themoviedb.org/3/search/movie",
+      {
+        params: {
+          query: `${title}`
+        },
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`
+        }
       }
-    }
-  ).then((response) => { return response.data })
+    ).then((response) => { return response.data })
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 export default requestAPI;
--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -7,33 +7,57 @@ import requestAPI from "../../common/services/movies";
 export default function SearchPage() {
   const [searchQuery, setSearchQuery] = useState("");
   const [queryResult, setQueryResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async () => {
+    setIsLoading(true);
     try {
       const result = await requestAPI(searchQuery);
-      setQueryResult(result);
+      if (result.results.length === 0) {
+        setQueryResult(null);
+      } else {
+        setQueryResult(result);
+        console.log(result);
+      }
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false);
   };
 
   return (
     <div className={styles["search-page"]}>
       <div className={styles.headling}>Find Your Movie</div>
       <div className={styles["search-wrapper"]}>
-        <input
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          type="text"
-          className={styles["search-bar"]}
-          placeholder="Lord of the rings"
-        ></input>
-        <button onClick={handleSubmit} className={styles["submit-btn"]}>
-          Enter
-        </button>
-        <div id="search-result-id">
-          {queryResult ? queryResult.results[0].title : ""}
+        <div className={styles["search-bar-wrapper"]}>
+          <input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            type="text"
+            className={styles["search-bar"]}
+            placeholder="Lord of the rings"
+          ></input>
+          <button onClick={handleSubmit} className={styles["submit-btn"]}>
+            Enter
+          </button>
         </div>
+        {isLoading && <div>Loading...</div>}
+        {queryResult && queryResult.results.length > 0 ? (
+          <div className={styles["search-result"]} id="search-result-id">
+            {queryResult.results
+              .filter((item) => item.poster_path)
+              .map((item, index) => (
+                <img
+                  key={index}
+                  className={styles["search-result__item"]}
+                  src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                  alt={item.title || "Movie Poster"}
+                />
+              ))}
+          </div>
+        ) : (
+          <div>No results</div>
+        )}
       </div>
     </div>
   );
