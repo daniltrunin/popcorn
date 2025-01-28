e5b9fcc83517af3c4655c60ccddd0dbf6c597005
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-01-28 19:05:04 +0500

    Details component layout improvements

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,6 +5,9 @@ export default function Details() {
   const location = useLocation();
   const cardDetails = location.state?.cardDetails;
   console.log(cardDetails);
+  const handleTrailerClick = (id) => {
+    window.open(`https://www.imdb.com/title/${id}`);
+  };
   return (
     <div className={styles.wrapper}>
       <div className={styles.sideber}>
@@ -23,37 +26,17 @@ export default function Details() {
             {cardDetails.genres.map((item) => item.name).join(", ")}
           </div>
           <div className={styles.tag}>{cardDetails.runtime} min</div>
+          <div
+            onClick={() => handleTrailerClick(cardDetails.imdb_id)}
+            className={`${styles.tag} ${styles.trailer}`}
+          >
+            Play trailer
+          </div>
         </div>
         <div className={styles.overview}>
           <h2 className={styles.headling}>Overview</h2>
           <p className={styles.description}>{cardDetails.overview}</p>
           <h2 className={styles.tagline}>{cardDetails.tagline}</h2>
-        </div>
-        <div>
-          <h3 className={styles.headling}>Production companies</h3>
-          <div className={styles.content}>
-            {cardDetails.production_companies
-              .map((item) => item.name)
-              .join(", ")}
-          </div>
-        </div>
-        <div className={styles["finance-wrapper"]}>
-          <h2 className={styles.headling}>About finance</h2>
-          <div className={styles.content}>
-            <div>
-              {cardDetails.budget ? `Budget ${cardDetails.budget} $` : ``}
-            </div>
-            <div>
-              {cardDetails.revenue ? `Revenue ${cardDetails.revenue} $` : ``}
-            </div>
-          </div>
-        </div>
-        <div className={styles.country}>
-          <h2 className={styles.headling}>Country and movie language</h2>
-          <div className={styles.content}>
-            {cardDetails.origin_country} &{" "}
-            {cardDetails.spoken_languages.map((item) => item.english_name)}
-          </div>
         </div>
       </div>
     </div>
