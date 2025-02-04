1c8df6c79de0d211923404091c8c3c6b88db958d
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-02-04 23:48:03 +0500

    Submit of Login and Register logic is in proccess

--- src/components/LoginWindow/LoginWindow.jsx
+++ src/components/LoginWindow/LoginWindow.jsx
@@ -1,6 +1,26 @@
 import styles from "./loginwindow.module.css";
+import { useState } from "react";
+
+function sendDataToLogin(username, password) {
+  const obj = {
+    user: username,
+    pass: password,
+  };
+  console.log(obj);
+}
+
+function sendDataToRegister(username, password) {
+  const obj = {
+    user: username,
+    pass: password,
+  };
+  console.log(obj);
+}
 
 export default function LoginWindow() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -8,14 +28,26 @@ export default function LoginWindow() {
           className={`${styles.input} ${styles.username}`}
           type="text"
           placeholder="Login"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           className={`${styles.input} ${styles.password}`}
           type="text"
           placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className={`${styles.button} ${styles.login}`}>Login</button>
-        <button className={`${styles.button} ${styles.register}`}>
+        <button
+          className={`${styles.button} ${styles.login}`}
+          type="submit"
+          onClick={() => sendDataToLogin(username, password)}
+        >
+          Login
+        </button>
+        <button
+          className={`${styles.button} ${styles.register}`}
+          type="submit"
+          onClick={() => sendDataToRegister(username, password)}
+        >
           Register
         </button>
       </div>
